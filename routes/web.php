a76f71e2f45028838eb2d19ade4d53df10a6c0bc
Yen Yon <yonyen99@gmail.com>
2023-10-25 08:56:06 +0700

    set up admin panel

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,16 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+$controller_path = 'App\Http\Controllers';
+
+// Main Page Route
+Route::get('/', $controller_path . '\dashboard\AnalyticsController@index')->name('dashboard-analytics');
+
+
+// cards
+Route::get('/cards/basic', $controller_path . '\cards\CardBasicController@index')->name('cards-basic');
