b62e27d83c7e4565c423a1d5af7f05f5c803664f
Yen Yon <yonyen99@gmail.com>
2023-12-24 22:00:08 +0700

    [imp] student activity resource

--- app/Http/Resources/StudentActivityResource.php
+++ app/Http/Resources/StudentActivityResource.php
@@ -6,21 +6,23 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class StudentActivityResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
-    public function toArray($request)
-    {
-      return [
-        'id' => $this->id,
-        'title' => $this->title,
-        'description' => $this->description,
-        'image' => $this->media[0]->image ?? null,
-        'created_at' => $this->created_at,
-        'updated_at' => $this->updated_at,
+  /**
+   * Transform the resource into an array.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+   */
+  public function toArray($request)
+  {
+    return [
+      'id' => $this->id,
+      'title' => $this->title,
+      'description' => html_entity_decode(strip_tags($this->description)),
+      'image' => $this->media[0]->image
+        ? asset('storage/' . $this->media[0]->image)
+        : null,
+      'created_at' => $this->created_at->format('Y-m-d'),
+      'updated_at' => $this->updated_at->format('Y-m-d'),
     ];
-    }
+  }
 }
