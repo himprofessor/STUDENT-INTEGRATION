698a153d5d0dbd8ea8731f1cf50a59b81ef396eb
SREYPHEAP.KHIN <SREYPHEAP.KHIN@PNC.LAN>
2023-12-26 15:11:38 +0700

    [api staff]

---
+++ app/Http/Resources/StaffResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StaffResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'email' => $this->email,
+            'position' => $this->position,
+            'media_id' => $this->media->image ? asset('storage/' . $this->media->image) :null,
+            'phone' => $this->phone,
+            'start_date' => $this->start_date,
+            'end_date' => $this->end_date,
+            'department' =>$this->department->department_name,
+            'about' => html_entity_decode(strip_tags($this->about)),
+            'created_at' => $this->created_at->format('Y-m-d'),
+            'updated_at' => $this->updated_at->format('Y-m-d'),
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\department_staff\StaffController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,4 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::prefix('student-activity')->group(function () {
   Route::get('/list', [StudentActivityController::class, 'index']);
+});
+
+Route::prefix('staff-list')->group(function () {
+  Route::get('/list', [StaffController ::class, 'index']);
 });
