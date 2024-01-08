4805208dddbd6ae4a245a08005fc6fde58c35b66
SOPHORN.TOEUM <SOPHORN.TOEUM@PNC.LAN>
2024-01-08 10:13:58 +0700

    [IMPROVEMENT] do all active menu bar in backend

--- resources/views/content/department/edit.blade.php
+++ resources/views/content/department/edit.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Department basic - UI elements')
 
 @section('vendor-script')
-    <script src="{{ asset('assets/vendor/libs/masonry/masonry.js') }}"></script>
+<script src="{{ asset('assets/vendor/libs/masonry/masonry.js') }}"></script>
 @endsection
 
 @section('page-script')
@@ -11,22 +11,22 @@
 @endsection
 
 @section('content')
-    <div class="d-flex justify-content-between align-items-center mb-4">
-        <h5 class="fw-bold">Department Update</h5>
-    </div>
-    <div class="row">
-        <div class="col-xl">
-            <div class="card mb-4">
-                <div class="card-body">
-                    <form method="POST" action="{{ route('department.edit',$department->id) }}" enctype="multipart/form-data">
-                        @csrf
-                        @method('PUT')
-                        @include('content.department.form', ['department' => $department])
-                        <a href="{{ url('department&staff/department') }}" class="btn btn-secondary">Cancel</a>
-                        <button type="submit" class="btn" style="background-color: #009DE1; color:white">Submit</button>
-                    </form>
-                </div>
+<div class="d-flex justify-content-between align-items-center mb-4">
+    <h5 class="fw-bold">Department Update</h5>
+</div>
+<div class="row">
+    <div class="col-xl">
+        <div class="card mb-4">
+            <div class="card-body">
+                <form method="POST" action="{{ url('/department&staff/department/edit/' . $department->id) }}" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
+                    @include('content.department.form', ['department' => $department])
+                    <a href="{{ url('department&staff/department') }}" class="btn btn-secondary">Cancel</a>
+                    <button type="submit" class="btn" style="background-color: #009DE1; color:white">Submit</button>
+                </form>
             </div>
         </div>
     </div>
+</div>
 @endsection
